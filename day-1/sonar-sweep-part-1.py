6c9550c068d431ddd8ec75ee42fcd6f52922972e
allliyah <74038500+allliyah@users.noreply.github.com>
2021-12-01 15:49:18 +0800

    Add solution for day 1 - part 1

---
+++ day-1/sonar-sweep-part-1.py
@@ -0,0 +1,16 @@
+def read_inputs(file_name):
+    input_file = open(file_name,"r")
+    nums = [int(line) for line in input_file]
+    return nums
+
+def main():
+    reports = read_inputs("input.txt")
+    ctr = 0
+    previous = reports[0]
+    for i in range(len(reports)):
+        if reports[i] > previous:
+            ctr+=1
+        previous = reports[i]
+    print(ctr)
+
+main()
