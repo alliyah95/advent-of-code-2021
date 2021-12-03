4adc9181ca7977f1379a9b7a6dceca7cccf3f98c
allliyah <74038500+allliyah@users.noreply.github.com>
2021-12-03 17:04:22 +0800

    Refactor solution

--- day-3/binary_diagnostic_part_1.py
+++ day-3/binary_diagnostic_part_1.py
@@ -3,22 +3,17 @@ def read_inputs(file_name):
     diagnostic_report = [r for r in input_file]
     return diagnostic_report
 
-
 def calculate(report):
-    #n:[zeroes:ones]
     data = {0:[0,0], 1:[0,0], 2:[0,0], 3:[0,0], 4:[0,0], 5:[0,0], 
     6:[0,0], 7:[0,0], 8:[0,0], 9:[0,0], 10:[0,0], 11:[0,0]}
 
     for r in report:
-        for i in range(len(r)):
-            if r[i] == "0": data[i][0]+=1
-            if r[i] == "1": data[i][1]+=1
+        for i in range(len(r)-1):
+            data[i][int(r[i])]+=1
 
     gamma_bin = "".join("0" if data[d][0] > data[d][1] else "1" for d in data)
     epsilon_bin = "".join("0" if data[d][0] < data[d][1] else "1" for d in data)
-
     return [gamma_bin, epsilon_bin]
-
 
 def main():
     report = read_inputs("input.txt")
