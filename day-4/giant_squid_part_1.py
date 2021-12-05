6e920d139766f8e14c68762a6ddc39420aaa4c4f
allliyah <74038500+allliyah@users.noreply.github.com>
2021-12-05 16:21:12 +0800

    Add final solution for day 4 - part 1

--- day-4/giant_squid_part_1.py
+++ day-4/giant_squid_part_1.py
@@ -2,7 +2,6 @@ def read_file(file_name):
     input_file = open(file_name, "r")
     return input_file.read().split("\n")
 
-
 def bingo(nums, boards):
     for n in nums:
         for x in range(len(boards)):
@@ -10,20 +9,34 @@ def bingo(nums, boards):
                 for z in range(len(boards[x][y])):
                     if boards[x][y][z] == n:
                         boards[x][y][z]+="T"
-                    if winning_row(boards[x][y]):
-                        pass
-                    elif winning_column(boards[x]):
-                        pass
+                    if has_winning_row(boards[x][y]) or has_winning_column(boards[x]):
+                        compute_final_score(boards[x], n)
                                  
+def has_winning_row(row):
+    return all("T" in r for r in row)
 
-def winning_row(row):
-    return all("T" in b for b in row)
-
-def winning_column(board):
+def has_winning_column(board):
+    col_num = 0
     col = []
     for i in range(len(board)):
-        for j in range(board[i]):
+        for b in board:
+            col.append(b[i])
+        if len(col) == len(board):
+            if all("T" in c for c in col):
+                return True
+            else:
+                col = []
+                col_num+=1
+    return False
 
+def compute_final_score(winning_board, winning_num):
+    total = 0
+    for row in winning_board:
+        for r in row:
+            if "T" not in r:
+                total+=int(r)
+    print(total*int(winning_num))
+    exit()
 
 def main():
     puzzle = read_file("input.txt")
